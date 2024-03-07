bc918ba4081245f1f4e308c95ab7559bf9fd4f84
Viaasyafrina16 <syafrinavia@gmail.com>
2024-03-07 15:54:03 +0700

    membuat procedur untuk mengurutkan array dengan metode bubble sort

--- praktikum 1/praktikum 1.cpp
+++ praktikum 1/praktikum 1.cpp
@@ -6,3 +6,46 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input() {
+	while (true) {
+		cout << "masukan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+
+		}
+	}
+	cout << endl;
+	cout << "===============" << endl;
+	cout << "masukkan Elemen Array" << endl;
+	cout << "===============" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+
+	}
+
+}
+
+void bublesortArray() { //procedur untuk mengurutkan array dengan metode bubble sort
+
+	int pass = 1; // step 1
+
+	do {
+		for (int j = 0; <= n - 1 - pass; j++) { // step 2
+			if (arr[j + 1]) { //step 3
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+
+
+			}
+		}
+		pass = pass + 1; //step 4
+
+	} while (pass < n - 1); //step 5
+
